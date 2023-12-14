d7e60a7ae1ecedfbad3b69e97b443304cf3c6103
Olisajioke <chijioke914@gmail.com>
2023-12-14 10:21:00 +0100

    merry_christmas

--- merry_christmas.py
+++ merry_christmas.py
@@ -3,8 +3,9 @@ import time
 message = "Because of His love and sacrifice, may all your heart desires this year come to pass, amen. Merry Christmas"
 
 
-for x in range(-11, 0):
+for x in range(-10, 0):
     time.sleep(1.00)
-    print(x)
+    print(abs(x))
 
+time.sleep(1.00)
 print(message)
